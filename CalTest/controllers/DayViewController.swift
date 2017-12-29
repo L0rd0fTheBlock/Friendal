86efd92f52f3678b0295d2b626d019f5c1bc0e1a
Jamie <jamie@jamies-air>
2017-12-29 02:35:41 +0000

    added drawEvent() which draws an event and attaches it as a subView of the tableView.
    Known Bug: event moves occasionally when scrolling and swiping back

--- CalTest/controllers/DayViewController.swift
+++ CalTest/controllers/DayViewController.swift
@@ -33,11 +33,55 @@ class DayViewController: UITableViewController {
         view.backgroundColor = .white
         //view.addSubview(tableView)
         //tableView.frame = CGRect(x: 0, y: 10, width: view.frame.width, height: view.frame.height)
+        for event in (today?.events)!{
+            drawEvent(event)
+        }
     }
+    
+    func drawEvent(_ event:Event){
+        
+        let start = makeMinutes(from: event.start!)
+        let end = makeMinutes(from: event.end!)
+        
+        let tableLength = 50*25
+        let breakdown = CGFloat(tableLength) / CGFloat(1440) //split the table into it's minutes
+        
+        let startPoint: CGFloat = breakdown * CGFloat(start) // multiplyy by the start time to push the event down the view
+        
+        print(start)
+        print(end)
+        print(CGFloat(end) - CGFloat(start))
+        print(startPoint)
+        print(startPoint + CGFloat(end))
+        
+        let frame = CGRect(x: CGFloat(30), y: startPoint, width: tableView.frame.width - 30, height: CGFloat(end) - CGFloat(start))
+        
+        let eventView = UIView(frame: frame)
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: eventView.frame.width, height: 30))
+        
+        label.text = event.title
+        
+        label.backgroundColor = UIColor.orange
+        
+        eventView.backgroundColor = UIColor.yellow.withAlphaComponent(0.55)
+        label.textAlignment = .center
+        
+        eventView.addSubview(label)
+        
+        tableView.addSubview(eventView)
+        
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         tabBarController?.tabBar.isHidden = false
         tableView.reloadData()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -113,4 +157,19 @@ class DayViewController: UITableViewController {
             navigationController?.pushViewController(eventView, animated: true)
         }
     }
+    
+    //MARK: Helper functions
+    
+    func makeMinutes(from: String) -> Int{
+        
+        let time = from.split(separator: ":")
+        
+        let htm:Int = Int(String(describing: time[0]))! * 60 //hour to minutes
+        
+        let minutes = htm + Int(String(describing: time[1]))!
+        
+        return minutes
+        
+    }
+    
 }
