eca62a612860d01b00a339dd74d01e9ec1498aa2
Jamie <jamie@jamies-air>
2017-12-31 22:29:34 +0000

    Added a TableViewCell with a toggle switch

---
+++ CalTest/cells/FormToggleCell.swift
@@ -0,0 +1,57 @@
+//
+//  FormToggleCell.swift
+//  CalTest
+//
+//  Created by Jamie McAllister on 31/12/2017.
+//  Copyright © 2017 Jamie McAllister. All rights reserved.
+//
+
+import UIKit
+
+class FormToggleCell: UITableViewCell {
+
+    let toggle = UISwitch()
+    let title = UILabel()
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        print(toggle.frame.width)
+        print(toggle.frame.height)
+        
+        let subtraction = toggle.frame.height / 2
+        
+        title.frame = CGRect(x: 10, y: 0, width: frame.width - 100, height: frame.height)
+        
+        toggle.frame = CGRect(x: frame.width - 70, y: (frame.height / 2) - subtraction, width: frame.width, height: frame.height)
+        
+        toggle.addTarget(self, action: #selector(didToggle), for: .touchUpInside)
+        
+        addSubview(toggle)
+        addSubview(title)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @objc
+    func didToggle(){
+        print(toggle.isOn)
+    }
+
+}
