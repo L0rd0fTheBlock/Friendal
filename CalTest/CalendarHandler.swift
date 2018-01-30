3698b257898d75256968ee91d990fd30dd9bb760
Jamie <jamie@jamies-macbook-air.local>
2018-01-30 15:19:20 +0000

    getNotGoing ported from GET to POST

--- CalTest/CalendarHandler.swift
+++ CalTest/CalendarHandler.swift
@@ -590,9 +590,21 @@ class CalendarHandler{
             
             var invitees: Array<Invitee> = []
             
-            let url = URL(string: self.BASE_URL + "/calendar/getNotGoing.php?id=" + forEvent)
+            let url = URL(string: self.BASE_URL + "/calendar/getNotGoing.php")//?id=" + forEvent)
             
-            let task = URLSession.shared.dataTask(with: url!){ (data, response, error) in
+            var request = URLRequest(url: url!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: TimeInterval(exactly: 10.00)!)
+            
+            request.httpMethod = "POST"
+            
+            var postString:String
+            
+            postString = "id=" + forEvent
+
+            
+            print(postString)
+            request.httpBody = postString.data(using: String.Encoding.utf8)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest){ (data, response, error) in
                 if error != nil {
                     print("ERROR")
                     DispatchQueue.main.async {
