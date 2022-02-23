eca7da211608b09db6126a691dd76a8c38f74c4a
Andy <andrew.macfarlane93@gmail.com>
2022-02-23 20:07:31 +0000

    Consolidated all Extension functions into one file and repurposed the Theme.swift file to dedicate it to store custom colours only

---
+++ CalTest/Extensions.swift
@@ -0,0 +1,64 @@
+//
+//  Extensions.swift
+//  CalTest
+//
+//  Created by Andrew McAllister on 23/02/2022.
+//  Copyright © 2022 MakeItForTheWeb Ltd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor{
+    
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat) {
+           assert(red >= 0 && red <= 255, "Invalid red component")
+           assert(green >= 0 && green <= 255, "Invalid green component")
+           assert(blue >= 0 && blue <= 255, "Invalid blue component")
+           assert(alpha >= 0 && alpha <= 1)
+
+           self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+       }
+
+       convenience init(rgb: Int) {
+           self.init(
+               red: (rgb >> 16) & 0xFF,
+               green: (rgb >> 8) & 0xFF,
+               blue: rgb & 0xFF,
+                alpha: 1
+           )
+       }
+    convenience init(rgb: Int, alpha: CGFloat) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF,
+            alpha: alpha
+        )
+    }
+}
+
+
+extension Date {
+    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+
+    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+        return calendar.component(component, from: self)
+    }
+}
+
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
--- CalTest/Theme.swift
+++ CalTest/Theme.swift
@@ -2,11 +2,22 @@
 //  Theme.swift
 //  CalTest
 //
-//  Created by Jamie McAllister on 18/06/2021.
-//  Copyright © 2021 Jamie McAllister. All rights reserved.
+//  Created by Andrew McAllister on 18/06/2021.
+//  Copyright © 2021 MakeItForTheWeb Ltd. All rights reserved.
 //
 
 import UIKit
+
+/*
+ 
+ The purpose of this file is to create colours that can be used within Palendar
+ 
+ It should not be used for anything else.
+ 
+ Any other functions extending UIColor should be placed inside the Extensions.swift file
+ 
+ 
+ */
 
 extension UIColor{
     public static var nav = UIColor(rgb: 0x33B0Df)
@@ -15,30 +26,4 @@ extension UIColor{
     public static var day = UIColor(rgb: 0x26be2d)
     public static var allDay = UIColor.darkGray
     public static var today = UIColor.lightGray
-    
-    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat) {
-           assert(red >= 0 && red <= 255, "Invalid red component")
-           assert(green >= 0 && green <= 255, "Invalid green component")
-           assert(blue >= 0 && blue <= 255, "Invalid blue component")
-           assert(alpha >= 0 && alpha <= 1)
-
-           self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-       }
-
-       convenience init(rgb: Int) {
-           self.init(
-               red: (rgb >> 16) & 0xFF,
-               green: (rgb >> 8) & 0xFF,
-               blue: rgb & 0xFF,
-                alpha: 1
-           )
-       }
-    convenience init(rgb: Int, alpha: CGFloat) {
-        self.init(
-            red: (rgb >> 16) & 0xFF,
-            green: (rgb >> 8) & 0xFF,
-            blue: rgb & 0xFF,
-            alpha: alpha
-        )
-    }
 }
